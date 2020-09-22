4e835050926f3fcbeebd41d4190e78d2e77f1f15
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-09-22 13:32:41 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const PORT = 4000;
+
+express()
+  .use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
+
+  .use(morgan("dev"))
+  .use(express.static("public"))
+  .use(bodyParser.json({ limit: "50mb" }))
+  .use(
+    bodyParser.urlencoded({
+      limit: "50mb",
+      extended: true,
+      parameterLimit: 50000,
+    })
+  )
+  .use("/", express.static(__dirname + "/"))
+
+  .use((req, res) => res.send("Not Found"))
+  .listen(PORT, () => console.log(`Listening on port ${PORT}`));
